2e6df6ccd70475c76ba2f860453e9bc620259954
pradoscript <prado.joao06480@gmail.com>
2025-04-26 20:19:38 -0300

    almost finalized the responsive

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -6,78 +6,79 @@ import { StaticImage } from "gatsby-plugin-image"
 export default function Contato() {
     return (
         <Layout>
-           <div className={styles.page}>
-           <section className={styles.mainContainer}>
-                <h1>Onde nos encontrar?</h1>
+            <div className={styles.page}>
+                <section className={styles.mainContainer}>
+               
                 <div className={styles.image}>
-                <StaticImage
-                    src="../../images/cafe-place.png"
-                    alt="Café Sem Filtro"
-                    placeholder="blurred"
-                    layout="constrained"
-                    quality={90}
-                    width={350}
-                />
-                </div>
-                <section className={styles.informations}>
-                    <div className={styles.informationsItems}>
+                <h1>Onde nos encontrar?</h1>
                         <StaticImage
-                        src="../../images/watch-asset.png"
-                        alt="Horários"
-                        placeholder="blurred"
-                        layout="constrained"
-                        quality={90}
+                            src="../../images/cafe-place.png"
+                            alt="Café Sem Filtro"
+                            placeholder="blurred"
+                            layout="constrained"
+                            quality={90}
+                            width={720}
                         />
-                        <p>
-                        06:00 -  19:00 <br/> Seg a Sáb.
-                        </p>
                     </div>
+                    <section className={styles.informations}>
+                        
+                        <div className={styles.informationsItems}>
+                        <section>
+                                <StaticImage
+                                    src="../../images/watch-asset.png"
+                                    alt="Horários"
+                                    placeholder="blurred"
+                                    layout="constrained"
 
-                    <div className={styles.informationsItems}>
-                        <StaticImage
-                        src="../../images/map-asset.png"
-                        alt="Horários"
-                        placeholder="blurred"
-                        layout="constrained"
-                        quality={90}
-                        />
-                        <p>
-                        Rua dos Grãos de Café <br/> CEP 12345-678
-                        </p>
-                    </div>
+                                />
+                            </section>
+                            <section>
+                                <p>
+                                    Segunda à Sabado <br/> 06h:00 às 19:00h
+                                </p>
+                            </section>
+                        </div>
+
+                        <div className={styles.informationsItems}>
+
+                            <section>
+                                <StaticImage
+                                    src="../../images/phone-asset.png"
+                                    alt="Horários"
+                                    placeholder="blurred"
+                                    layout="constrained"
+
+                                />
+                            </section>
+                            <section>
+                                <p>
+                                (11) 99140-7988
+                                </p>
+                            </section>
+
+                        </div>
+
+                        <div className={styles.informationsItems}>
+                        <section>
+                                <StaticImage
+                                    src="../../images/map-asset.png"
+                                    alt="Horários"
+                                    placeholder="blurred"
+                                    layout="constrained"
+
+                                />
+                            </section>
+                            <section>
+                                <p>
+                                    Rua dos Grãos de Café <br /> CEP 12345-678
+                                </p>
+                            </section>
+                        </div>
+
+                    </section>
+                    
                 </section>
-            </section>
-
-            <section className={styles.contact}>
-                <div className={styles.zap}>
-                <section>
-                    <StaticImage
-                        src="../../images/zap-asset.png"
-                        alt="Whatsapp"
-                        placeholder="blurred"
-                        layout="constrained"
-                        quality={100}
-                        width={60}
-                />
-                <p>  (11) 99140-7988 </p>
-                    </section>
-                </div>
-
-                <div className={styles.insta}>
-                    <section>
-                    <StaticImage
-                        src="../../images/insta-asset.png"
-                        alt="Horários"
-                        placeholder="blurred"
-                        layout="constrained"
-                        quality={100}
-                        width={60}
-                />
-                <p> @CAFESEMFILTRO </p>
-                    </section>
-                </div>
-            </section>
-           </div>
+            </div>
         </Layout>
     )
 }
