93956a4031a80d599afdfa81704c130278283894
pradoscript <prado.joao06480@gmail.com>
2025-04-27 02:09:03 -0300

    adding meta and title (must add more component to finalize) and changing width

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Café Sem Filtro`,
+    description: `A melhor cafeteria de São Carlos e região!`,
+    author: `@pradojoao`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
